5762de7abf3d5e3f4c13aaaa28147aba951393e6
kuldarvakker <kuvakk@taltech.ee>
2022-03-21 20:10:55 +0200

    added equals & hashcode to burgerjoint

--- src/main/java/com/qminder/borger/app/domain/BurgerJoint.java
+++ src/main/java/com/qminder/borger/app/domain/BurgerJoint.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -22,4 +23,17 @@ public class BurgerJoint {
 
     private LocalDateTime createdAt;
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BurgerJoint that = (BurgerJoint) o;
+        return id == that.id && Objects.equals(fsqId, that.fsqId) && Objects.equals(name, that.name) && Objects.equals(longitude, that.longitude) && Objects.equals(latitude, that.latitude) && Objects.equals(createdAt, that.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fsqId, name, longitude, latitude, createdAt);
+    }
 }
